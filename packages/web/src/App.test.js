e24280eff61dc2a2ba82d9690f5abe57dd38ae77
Micael Souza <micael.souza@outlook.com>
2020-08-03 19:48:57 -0300

    Add WebApp boilerplate

---
+++ packages/web/src/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import App from './App';
+
+describe('A suite', () => {
+  it('should render without throwing an error', () => {
+    expect(shallow(<App />).contains(<div className="app">Hello World</div>)).toBe(true);
+  });
+
+  it('should be selectable by class "app"', () => {
+    expect(shallow(<App />).is('.app')).toBe(true);
+  });
+
+  it('should mount in a full DOM', () => {
+    expect(mount(<App />).find('.app').length).toBe(1);
+  });
+
+  it('should render to static HTML', () => {
+    expect(render(<App />).text()).toEqual('Hello World');
+  });
+});
